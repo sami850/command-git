ba6016fb5cc3565245c0988f659a21d0a32f8d57
sammir <saman.mirsalari@gmail.com>
2022-09-10 18:06:00 +0430

    currect commit message

---
+++ resources/views/logs.blade.php
@@ -0,0 +1,20 @@
+@extends('master')
+
+@section('content')
+
+
+    <div class="container" >
+        <div class="row">
+            <div class="col-md-9 col-7 mt-4 mb-4 pt-md-4 pr-0 mb-md-0">
+                <hr />
+            </div>
+            <div class="col-md-3 mt-md-4 pt-md-2 col-5 mt-4 mb-4 mb-md-0 pl-0">
+                <img  class=" "  src='{{asset("assets/images/aviz-khti2.png")}}' >
+
+q
+                ssssssssssssssssssssssssssssssss
+            </div>
+        </div>
+    </div>
+
+@endsection
